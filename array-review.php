623fe859cbfad5232f4a4c8ad86b34e769bfed0d
Ajwinder Singh <asinghgr@srv601.webhostingforstudents.com>
2018-01-16 06:08:54 -0500

    Added function, addAnimal

--- array-review.php
+++ array-review.php
@@ -11,3 +11,27 @@ function sortAndPrint ()
     }
     echo "<br>";
 }
+
+function addAnimal ($str)
+{
+    $animals = array('Panda', 'alpaca', 'boa');
+    echo "adding " . $str . ". . .<br>";
+
+    $animals = array_map('strtolower', $animals);
+    $str = strtolower($str);
+
+    if (!in_array($str, $animals))
+    {
+        array_push($animals, $str);
+    }
+    else
+    {
+        echo "Array element is already present;";
+        echo "<br>";
+
+    }
+    for ($i = 0; $i <= sizeof($animals); $i++ )
+    {
+        echo $animals[$i].' ';
+    }
+}
