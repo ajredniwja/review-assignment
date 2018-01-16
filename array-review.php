b41289ec1125765c92980ccbc4ef9ea78bc030bf
Ajwinder Singh <asinghgr@srv601.webhostingforstudents.com>
2018-01-16 06:33:36 -0500

    Started part 2, defined associative array

--- array-review.php
+++ array-review.php
@@ -1,5 +1,7 @@
 <?php
 
+//PART 1
+echo "<h3>Part 1</h3>h3>";
 function sortAndPrint ()
 {
     $animals = array('panda', 'alpaca', 'boa');
@@ -38,3 +40,8 @@ function addAnimal ($str)
 
 sortAndPrint();
 addAnimal('Alpaca');
+
+//PART 2
+
+$flavors = array("grasshoper"=>"The Grasshopper", "maple"=>"Whiskey Maple Bacon", "carrot"=>"Carrot Walnut", "caramel"=>"Salted Caramel Cupcake", "velvet"=>"Red Velvet", "lemon"=>"Lemon Drop", "tiramisu"=>"Tiramisu");
+echo "<br>";
