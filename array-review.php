fa50f40b6c80a04ad941422214ff63bcfb6058a4
Ajwinder Singh <asinghgr@srv601.webhostingforstudents.com>
2018-01-16 06:06:45 -0500

    Added function, sortAndPrint

--- array-review.php
+++ array-review.php
@@ -1,3 +1,13 @@
 <?php
 
+function sortAndPrint ()
+{
+    $animals = array('panda', 'alpaca', 'boa');
+    sort($animals);
 
+    for ($i = 0; $i <= sizeof($animals); $i++ )
+    {
+        echo ($animals[$i]." ");
+    }
+    echo "<br>";
+}
