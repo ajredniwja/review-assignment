3cd40a1dad68c98cb90441ea3da7ceca91d33401
Ajwinder Singh <asinghgr@srv601.webhostingforstudents.com>
2018-01-16 09:41:52 -0500

    Final commit

--- array-review.php
+++ array-review.php
@@ -1,7 +1,7 @@
 <?php
 
 //PART 1
-echo "<h3>Part 1</h3>h3>";
+echo "<h3>Part 1</h3>";
 function sortAndPrint ()
 {
     $animals = array('panda', 'alpaca', 'boa');
@@ -25,24 +25,24 @@ function addAnimal ($str)
     if (!in_array($str, $animals))
     {
         array_push($animals, $str);
+        for ($i = 0; $i <= sizeof($animals); $i++ )
+        {
+            echo $animals[$i].' ';
+        }
     }
     else
     {
-        echo "Array element is already present;";
+        echo "Element is already present in the array;";
         echo "<br>";
 
-    }
-    for ($i = 0; $i <= sizeof($animals); $i++ )
-    {
-        echo $animals[$i].' ';
     }
 }
 
 sortAndPrint();
 addAnimal('Alpaca');
 
-//PART 2
 
+echo "<h3>Part 2 </h3>";
 $flavors = array("grasshoper"=>"The Grasshopper", "maple"=>"Whiskey Maple Bacon", "carrot"=>"Carrot Walnut", "caramel"=>"Salted Caramel Cupcake", "velvet"=>"Red Velvet", "lemon"=>"Lemon Drop", "tiramisu"=>"Tiramisu");
 echo "<br>";
 foreach ($flavors as $x => $x_value)
