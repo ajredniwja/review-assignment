578fb4db054d375cabd29846db0394f646943998
Ajwinder Singh <asinghgr@srv601.webhostingforstudents.com>
2018-01-16 20:24:12 -0500

    changed and ready for the final submit to teacher

--- array-review.php
+++ array-review.php
@@ -2,46 +2,45 @@
 
 //PART 1
 echo "<h3>Part 1</h3>";
-function sortAndPrint ()
+$animals = array('panda', 'alpaca', 'boa'); //defined array
+
+
+//function to sort and print the array
+function sortAndPrint ($animals)
 {
-    $animals = array('panda', 'alpaca', 'boa');
+
     sort($animals);
 
     for ($i = 0; $i <= sizeof($animals); $i++ )
     {
         echo ($animals[$i]." ");
     }
-    echo "<br>";
 }
 
-function addAnimal ($str)
+//funtion to add an element
+function addAnimal ($animals, $str)
 {
-    $animals = array('Panda', 'alpaca', 'boa');
-    echo "adding " . $str . ". . .<br>";
+    echo "<br>adding " . $str . ". . .<br>";
 
     $animals = array_map('strtolower', $animals);
     $str = strtolower($str);
-
-    if (!in_array($str, $animals))
+    if (!in_array($str,$animals))
     {
         array_push($animals, $str);
-        for ($i = 0; $i <= sizeof($animals); $i++ )
-        {
-            echo $animals[$i].' ';
-        }
     }
-    else
-    {
-        echo "Element is already present in the array;";
-        echo "<br>";
+    sort($animals);
 
-    }
+    return $animals;
 }
 
-sortAndPrint();
-addAnimal('Alpaca');
+sortAndPrint ($animals);
 
+$animals = addAnimal($animals, 'goat');
+sortAndPrint($animals);
+$animals = addAnimal($animals, 'Boa');
+sortAndPrint($animals);
 
+//part 2
 echo "<h3>Part 2 </h3>";
 $flavors = array("grasshoper"=>"The Grasshopper", "maple"=>"Whiskey Maple Bacon", "carrot"=>"Carrot Walnut", "caramel"=>"Salted Caramel Cupcake", "velvet"=>"Red Velvet", "lemon"=>"Lemon Drop", "tiramisu"=>"Tiramisu");
 echo "<br>";
